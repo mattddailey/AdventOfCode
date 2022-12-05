555073d9cd4e615f9aa9fe6ece85a079035f55a2
mdaile524 <matthew_dailey@comcast.com>
2022-12-05 14:53:47 -0500

    Cleanup Day 5

--- AdventOfCode2022.playground/Pages/dec05.xcplaygroundpage/Contents.swift
+++ AdventOfCode2022.playground/Pages/dec05.xcplaygroundpage/Contents.swift
@@ -1,12 +1,12 @@
 import Foundation
 
-func part1() -> String {
-    
+typealias StacksDict = [Int: [Character]]
+
+func parseInput(_ input: [String]) -> (StacksDict, [String]) {
     var stacksDict = [Int: [Character]]()
     var instructions: [String] = []
     
-    let helper = InputHelper(fileName: "dec05Input")
-    helper.inputAsArraySeparatedBy(.newlines)
+    input
         .forEach { row in
             // Build stacksDict
             if row.contains("[") {
@@ -30,23 +30,10 @@ func part1() -> String {
             }
         }
     
-    // loop through instructions, move items accordingly using removeFirst & append
-    instructions.forEach { instruction in
-        let readableInstructions = instruction.components(separatedBy: .whitespaces)
-            .compactMap { Int($0) }
-        let numberToMove = readableInstructions[0]
-        let source = readableInstructions[1]
-        let destination = readableInstructions[2]
-        
-        for _ in 1...numberToMove {
-            if let character = stacksDict[source]?.removeFirst() {
-                stacksDict[destination]?.insert(character, at: 0)
-            }
-        }
-    }
-    
-    
-    // sort dict, loop through and create string of elements on top
+    return (stacksDict, instructions)
+}
+
+func buildOutputString(_ stacksDict: StacksDict) -> String {
     let sortedStack = stacksDict.sorted(by: { $0.0 < $1.0 })
     var result = ""
     for pair in sortedStack {
@@ -55,73 +42,66 @@ func part1() -> String {
         }
         
     }
-
     return result
 }
+
+struct Instruction {
+    let numberToMove: Int
+    let source: Int
+    let destination: Int
+    
+    init(instruction: String) {
+        let integers = instruction.components(separatedBy: .whitespaces)
+            .compactMap { Int($0) }
+        self.numberToMove = integers[0]
+        self.source = integers[1]
+        self.destination = integers[2]
+    }
+}
+
+//MARK: - Part 1
+
+func part1() -> String {
+    let helper = InputHelper(fileName: "dec05Input")
+    var (stacksDict, instructions) = parseInput(helper.inputAsArraySeparatedBy(.newlines))
+    
+    // loop through instructions, move items accordingly
+    instructions
+        .map(Instruction.init)
+        .forEach { instruction in
+            for _ in 1...instruction.numberToMove {
+                if let character = stacksDict[instruction.source]?.removeFirst() {
+                    stacksDict[instruction.destination]?.insert(character, at: 0)
+            }
+        }
+    }
+    
+    return buildOutputString(stacksDict)
+}
+
+//MARK: - Part 2
 
 func part2() -> String {
-    var stacksDict = [Int: [Character]]()
-    var instructions: [String] = []
-    
     let helper = InputHelper(fileName: "dec05Input")
-    helper.inputAsArraySeparatedBy(.newlines)
-        .forEach { row in
-            // Build stacksDict
-            if row.contains("[") {
-                var index = 1
-                var stackNumber = 1
-                while index < row.count {
-                    let character = row[index]
-                    if character.isLetter {
-                        if stacksDict[stackNumber] != nil {
-                            stacksDict[stackNumber]?.append(character)
-                        } else {
-                            stacksDict[stackNumber] = [character]
-                        }
-                    }
-                    stackNumber += 1
-                    index += 4
+    var (stacksDict, instructions) = parseInput(helper.inputAsArraySeparatedBy(.newlines))
+    
+    // loop through instructions, move items accordingly
+    instructions
+        .map(Instruction.init)
+        .forEach { instruction in
+            var tempArray: [Character] = []
+            for _ in 1...instruction.numberToMove {
+                if let character = stacksDict[instruction.source]?.removeFirst() {
+                    tempArray.append(character)
                 }
-            } else if row.contains("move") {
-                // Get instructions
-                instructions.append(row)
+            }
+            tempArray.reversed().forEach {
+                stacksDict[instruction.destination]?.insert($0, at: 0)
             }
         }
     
-    // loop through instructions, move items accordingly using removeFirst & append
-    instructions.forEach { instruction in
-        let readableInstructions = instruction.components(separatedBy: .whitespaces)
-            .compactMap { Int($0) }
-        let numberToMove = readableInstructions[0]
-        let source = readableInstructions[1]
-        let destination = readableInstructions[2]
-        
-        var tempArray: [Character] = []
-        for _ in 1...numberToMove {
-            if let character = stacksDict[source]?.removeFirst() {
-                tempArray.append(character)
-            }
-        }
-        tempArray.reversed().forEach {
-            stacksDict[destination]?.insert($0, at: 0)
-        }
-    }
-    
-    
-    // sort dict, loop through and create string of elements on top
-    let sortedStack = stacksDict.sorted(by: { $0.0 < $1.0 })
-    var result = ""
-    for pair in sortedStack {
-        if let character = pair.value.first {
-            result = result + String(character)
-        }
-        
-    }
-
-    return result
+    return buildOutputString(stacksDict)
 }
-
-
 
 print(part1())
 print(part2())
