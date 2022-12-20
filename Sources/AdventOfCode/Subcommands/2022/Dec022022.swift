22e0b35c5a6edcc3252764dac9c22a25387d8df5
mdaile524 <matthew_dailey@comcast.com>
2022-12-20 13:51:59 -0500

    Add placegolder files

---
+++ Sources/AdventOfCode/Subcommands/2022/Dec022022.swift
@@ -0,0 +1,103 @@
+import ArgumentParser
+
+struct Dec022022: ParsableCommand, AOCDay {
+    static let configuration = CommandConfiguration(abstract: "Advent of Code - 2022 December 02", version: "1.0.0")
+
+    @Option(name: .shortAndLong, help: "Input file path")
+    var path: String = "Input/dec022022.txt"
+
+    // MARK: - Data Structures
+
+    enum Choice {
+        case rock
+        case paper
+        case scissor
+    
+        var score: Int {
+            switch self {
+            case .rock:
+                return 1
+            case .paper:
+                return 2
+            case .scissor:
+                return 3
+            }
+        }
+    }
+
+    enum Outcome {
+        case win
+        case loss
+        case draw
+        
+        var score: Int {
+            switch self {
+            case .win:
+                return 6
+            case .loss:
+                return 0
+            case .draw:
+                return 3
+            }
+        }
+    }
+
+    var part1Scores: [String : Int] = [
+        // LHS: input ; RHS: score of our play + score of outcome
+        "A X" : Choice.rock.score + Outcome.draw.score,
+        "A Y" : Choice.paper.score + Outcome.win.score,
+        "A Z" : Choice.scissor.score + Outcome.loss.score,
+        "B X" : Choice.rock.score + Outcome.loss.score,
+        "B Y" : Choice.paper.score + Outcome.draw.score,
+        "B Z" : Choice.scissor.score + Outcome.win.score,
+        "C X" : Choice.rock.score + Outcome.win.score,
+        "C Y" : Choice.paper.score + Outcome.loss.score,
+        "C Z" : Choice.scissor.score + Outcome.draw.score,
+    ]
+
+    var part2Scores: [String : Int] = [
+        // LHS: input ; RHS: score of our play + score of outcome
+        "A X" : Choice.scissor.score + Outcome.loss.score,
+        "A Y" : Choice.rock.score + Outcome.draw.score,
+        "A Z" : Choice.paper.score + Outcome.win.score,
+        "B X" : Choice.rock.score + Outcome.loss.score,
+        "B Y" : Choice.paper.score + Outcome.draw.score,
+        "B Z" : Choice.scissor.score + Outcome.win.score,
+        "C X" : Choice.paper.score + Outcome.loss.score,
+        "C Y" : Choice.scissor.score + Outcome.draw.score,
+        "C Z" : Choice.rock.score + Outcome.win.score,
+    ]
+
+    mutating func run() throws {
+        let lines  = try String(contentsOfFile: path).components(separatedBy: .newlines)
+
+        print("Part 1: \(part1(lines))")
+        print("Part 2: \(part2(lines))")
+    }
+
+    // MARK: - Part 1
+
+    func part1(_ lines: [String]) -> Int {
+        var score = 0
+        for round in lines {
+            if !round.isEmpty {
+                score += part1Scores[round] ?? 0
+            }
+        }
+        
+        return score
+    }
+
+    // MARK: - Part 2
+
+    func part2(_ lines: [String]) -> Int {
+         var score = 0
+        for round in lines {
+            if !round.isEmpty {
+                score += part2Scores[round] ?? 0
+            }
+        }
+        
+        return score
+    }
+}
---
+++ Sources/AdventOfCode/Subcommands/2022/Dec082022.swift
@@ -0,0 +1,31 @@
+import ArgumentParser
+
+struct Dec082022: ParsableCommand, AOCDay {
+    static let configuration = CommandConfiguration(abstract: "Advent of Code - 2022 December 08", version: "1.0.0")
+
+    @Option(name: .shortAndLong, help: "Input file path")
+    var path: String = "Input/dec082022.txt"
+
+    // MARK: - Data Structures
+
+    // MARK: - Lifecycle
+
+    mutating func run() throws {
+        let lines  = try String(contentsOfFile: path).components(separatedBy: .newlines)
+
+        print("Part 1: \(part1(lines))")
+        print("Part 2: \(part2(lines))")
+    }
+
+    // MARK: - Part 1
+
+    func part1(_ lines: [String]) -> Int {
+        return 0
+    }
+
+    // MARK: - Part 2
+
+    func part2(_ lines: [String]) -> Int {
+        return 0
+    }
+}
