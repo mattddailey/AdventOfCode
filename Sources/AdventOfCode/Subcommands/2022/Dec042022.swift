096a1724148500c83a541b388354947d35cdafa3
mdaile524 <matthew_dailey@comcast.com>
2022-12-21 16:21:24 -0500

    Working Part 1

--- Sources/AdventOfCode/Subcommands/2022/Dec042022.swift
+++ Sources/AdventOfCode/Subcommands/2022/Dec042022.swift
@@ -8,6 +8,24 @@ struct Dec042022: ParsableCommand, AOCDay {
 
     // MARK: - Data Structures
 
+    struct SectionAssignment {
+    let lower: Int
+    let upper: Int
+    
+    init?(_ asString: String?) {
+        let components = asString?.components(separatedBy: "-")
+        guard let lower = components?.first, !lower.isEmpty,
+              let upper = components?.last, !upper.isEmpty
+        else { return nil }
+            self.lower = Int(lower) ?? 0
+            self.upper = Int(upper) ?? 0
+        }
+        
+        var asSet: Set<Int> {
+            return Set(lower...upper)
+        }
+    }
+
     // MARK: - Lifecycle
 
     mutating func run() throws {
@@ -20,12 +38,48 @@ struct Dec042022: ParsableCommand, AOCDay {
     // MARK: - Part 1
 
     func part1(_ lines: [String]) -> Int {
-        return 0
+        let reconsiderCount = lines
+            .compactMap(convertToSets)
+            .compactMap(isFullyContained)
+            .count
+        
+        return reconsiderCount
     }
 
     // MARK: - Part 2
 
     func part2(_ lines: [String]) -> Int {
-        return 0
+        let overlapCount = lines
+            .compactMap(convertToSets)
+            .map(containsOverlap)
+            .filter { $0 }
+            .count
+        
+        return overlapCount
+    }
+
+    func containsOverlap(_ sets: [Set<Int>]) -> Bool {
+        return !sets[0].isDisjoint(with: sets[1])
+    }
+
+    func isFullyContained(_ sets: [Set<Int>]) -> Bool? {
+        if (sets[0].isSubset(of: sets[1])) ||
+            (sets[1].isSubset(of: sets[0])) {
+            return true
+        } else {
+            return nil
+        }
+    }
+
+    func convertToSets(_ pair: String) -> [Set<Int>]? {
+        let elves = pair.components(separatedBy: ",")
+        let firstElf = SectionAssignment(elves.first)
+        let secondElf = SectionAssignment(elves.last)
+        
+        if let firstElf = firstElf, let secondElf = secondElf {
+            return [firstElf.asSet, secondElf.asSet]
+        } else {
+            return nil
+        }
     }
 }
--- Sources/AdventOfCode/Subcommands/2022/Dec072022.swift
+++ Sources/AdventOfCode/Subcommands/2022/Dec072022.swift
@@ -1,12 +1,115 @@
 import ArgumentParser
 
+let ELFDEVICESPACE = 70000000
+let UPDATESIZE = 30000000
+
 struct Dec072022: ParsableCommand, AOCDay {
-    static let configuration = CommandConfiguration(abstract: "Advent of Code - 2022 December 07", version: "1.0.0")
+    static let configuration = CommandConfiguration(abstract: "Advent of Code - No Space Left On Device", version: "1.0.0")
 
     @Option(name: .shortAndLong, help: "Input file path")
     var path: String = "Input/dec072022.txt"
 
     // MARK: - Data Structures
+
+    struct File {
+        let name: String
+        let size: Int
+    }
+
+    class Directory {
+        let name: String
+        var files: [File] = []
+        var subDirectories: [String : Directory] = [:]
+        var parent: Directory? = nil
+        
+        init(name: String, subDirectories: [String : Directory] = [:], parent: Directory? = nil) {
+            self.name = name
+            self.subDirectories = subDirectories
+            self.parent = parent
+        }
+        
+        var size: Int {
+            var size = 0
+            subDirectories.values.forEach { size += $0.size }
+            files.forEach { size += $0.size }
+            return size
+        }
+    }
+
+    struct ElfDevice {
+        
+        var root: Directory = {
+            let home = Directory(name: "/")
+            let root = Directory(name: "root", subDirectories: ["/" : home])
+            home.parent = root
+            return root
+        }()
+        
+        let terminalLines: [String]
+        var maxDirectorySizeForDelete = 100000
+        var spaceToFree = 0
+        var deleteSize = 70000000
+        
+        mutating func buildDirectory(_ commandIndex: Int = 0, directory: Directory?) {
+            guard commandIndex < terminalLines.count else { return }
+            
+            let command = terminalLines[commandIndex]
+            let nextIndex = commandIndex + 1
+            let components = command.components(separatedBy: .whitespaces)
+            
+            if let size = Int(components[0]) {
+                // parse file
+                let file = File(name: components[1], size: size)
+                directory?.files.append(file)
+                buildDirectory(nextIndex, directory: directory)
+            } else {
+                if components[0] == "dir" {
+                    // parse subdirectory
+                    let subDirectory = Directory(name: components[1], parent: directory)
+                    directory?.subDirectories[components[1]] = subDirectory
+                    buildDirectory(nextIndex, directory: directory)
+                } else {
+                    if components[1] == "cd" {
+                        // going back to parent
+                        if components[2] == ".." {
+                            buildDirectory(nextIndex, directory: directory?.parent)
+                        } else {
+                            // going into a subdirectory
+                            buildDirectory(nextIndex, directory: directory?.subDirectories[components[2]])
+                        }
+                    } else {
+                        // ls command (pass)
+                        buildDirectory(nextIndex, directory: directory)
+                    }
+                }
+            }
+        }
+        
+        mutating func getSmallestSizeToDelete(_ directory: Directory) -> Int {
+            let size = directory.size
+            if size >= spaceToFree && size < deleteSize {
+                deleteSize = size
+            }
+            directory.subDirectories.values.forEach {
+                if getSmallestSizeToDelete($0) < deleteSize {
+                    deleteSize = size
+                }
+            }
+            return deleteSize
+        }
+        
+        func getDeletableSize(_ directory: Directory) -> Int {
+            var size = 0
+            directory.subDirectories.values.forEach {
+                let dirSize = $0.size
+                if dirSize <= maxDirectorySizeForDelete {
+                    size += dirSize
+                }
+                size += getDeletableSize($0)
+            }
+            return size
+        }
+    }
 
     // MARK: - Lifecycle
 
@@ -20,12 +123,20 @@ struct Dec072022: ParsableCommand, AOCDay {
     // MARK: - Part 1
 
     func part1(_ lines: [String]) -> Int {
-        return 0
+        var device = ElfDevice(terminalLines: lines)
+        device.buildDirectory(0, directory: device.root)
+    
+        return device.getDeletableSize(device.root)
     }
 
     // MARK: - Part 2
 
     func part2(_ lines: [String]) -> Int {
-        return 0
+        var device = ElfDevice(terminalLines: lines)
+        device.buildDirectory(directory: device.root)
+        let unusedSpace = ELFDEVICESPACE - (device.root.size)
+        device.spaceToFree = UPDATESIZE - unusedSpace
+        
+        return device.getSmallestSizeToDelete(device.root)
     }
 }
