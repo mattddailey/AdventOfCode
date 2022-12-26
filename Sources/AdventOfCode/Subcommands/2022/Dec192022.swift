1794efa20386d6c3fb763d5cd2623a9af4f741d3
mdaile524 <matthew_dailey@comcast.com>
2022-12-26 09:33:55 -0500

    Working Day 9

--- Sources/AdventOfCode/Subcommands/2022/Dec192022.swift
+++ Sources/AdventOfCode/Subcommands/2022/Dec192022.swift
@@ -8,168 +8,61 @@ struct Dec192022: ParsableCommand, AOCDay {
 
     // MARK: - Data Structures
 
-    typealias Blueprint = [Material : Robot]
+    typealias Blueprint = [Material : Cost]
+    typealias Cost = [Material : Int]
     typealias Robots = [Material : Int]
+    typealias Maxes = [Material : Int]
+    typealias Stock = [Material : Int]
 
-    enum Material: String {
+    enum Material: String, CaseIterable {
         case ore
         case clay
         case obsidian
         case geode
     }
 
-    struct Robot: Hashable {
-        let type: Material
-        let cost: [Material : Int]
-
-        init(type: Material, cost: [Material: Int] = [:]) {
-            self.type = type
-            self.cost = cost
-        }
+    struct State: Hashable {
+        var time: Int
+        var robots: Robots
+        var stock: Stock
+        var skipped: Set<Material>
     }
-
-    struct Stock {
-        var ore: Int = 0
-        var clay: Int = 0
-        var obsidian: Int = 0
-        var geode: Int = 0
-    }
-
-    class GeodeMiner {
-        let blueprint: Blueprint
-        let maxes: [Material : Int]
-
-        var maxGeodes = 0
-
-        init(blueprint: Blueprint, maxes: [Material : Int]) {
-            self.blueprint = blueprint
-            self.maxes = maxes
-        }
-
-        // will ultimately return the largest number of geodes that can be mined
-        func mine(robots: Robots, stock: Stock, time: Int) -> Int {
-            guard time > 0 && canBeatMax(stock.geode, time) else { 
-                maxGeodes = max(maxGeodes, stock.geode)
-                return stock.geode 
-            }
-
-            // print("== Minute \(25 - time) ==")
-            let affordableRobots = blueprint
-                .filter { canBuild($0.value, stock) && shouldBuild($0.value, robots, stock) }
-
-            // collect minerals from current robots
-            let mutableStock = collect(robots, stock)
-
-            let maxWhenBuildingARobot = affordableRobots
-                .map { pair in
-                    let (mutableRobots, mutableStock) = build(pair.value, robots, stock)
-                    return mine(robots: mutableRobots, stock: mutableStock, time: time - 1) 
-                }
-                .max() ?? 0
-            let noNewRobotsResult = mine(robots: robots, stock: mutableStock, time: time - 1)
-            return max(maxWhenBuildingARobot, noNewRobotsResult)
-        }
-
-        private func build(_ robot: Robot, _ robots: Robots, _ stock: Stock) -> (Robots, Stock) {
-            var mutableStock = stock
-            for material in robot.cost.keys {
-                guard let cost = robot.cost[material] else { continue }
-                switch material {
-                case .ore: mutableStock.ore -= cost
-                case .clay: mutableStock.clay -= cost
-                case .obsidian: mutableStock.obsidian -= cost
-                case .geode: mutableStock.geode -= cost
-                }
-            }
-
-            var mutableRobots = robots
-            let currentCount = robots[robot.type] ?? 0
-            // print("Spend \(robot.cost) to start building a new \(robot.type) robot.")
-            mutableRobots[robot.type] = currentCount + 1
-            // print("The new \(robot.type) robot is ready. You now have \(mutableRobots[robot.type] ?? 0) of them")
-
-            return (mutableRobots, mutableStock)
-        }
-
-        private func canBeatMax(_ currentGeodes: Int, _ time: Int) -> Bool {
-            return time - 1 + currentGeodes > maxGeodes
-        }
-
-        private func canBuild(_ robot: Robot, _ stock: Stock) -> Bool {
-            switch robot.type {
-            case .ore:      return stock.ore >= (robot.cost[.ore] ?? Int.max) 
-            case .clay:     return stock.ore >= (robot.cost[.ore] ?? Int.max) 
-            case .obsidian: return (stock.ore >= (robot.cost[.ore] ?? Int.max) && stock.clay >= (robot.cost[.clay] ?? Int.max))
-            case .geode:    return (stock.ore >= (robot.cost[.ore] ?? Int.max) && stock.obsidian >= (robot.cost[.obsidian] ?? Int.max))
-            }
-        }
-
-        private func shouldBuild(_ robot: Robot, _ robots: Robots, _ stock: Stock) -> Bool {
-            guard robot.type != .geode else { return true }
-
-            guard let numberCurrentlyProducing = robots[robot.type],
-                  let maximum = maxes[robot.type] 
-            else { return true }
-
-
-            return numberCurrentlyProducing < maximum
-        }
-
-        private func collect(_ robots: Robots, _ stock: Stock) -> Stock {
-            var mutableStock = stock
-            for material in robots.keys {
-                guard let count = robots[material] else { continue }
-                switch material {
-                case .ore: 
-                    mutableStock.ore += count     
-                    // print("\(count) ore-collecting robot collects \(count) ore; you now have \(mutableStock.ore) ore.")
-                case .clay:     
-                    mutableStock.clay += count     
-                    // print("\(count) clay-collecting robot collects \(count) clay; you now have \(mutableStock.clay) clay.")
-                case .obsidian: 
-                    mutableStock.obsidian += count     
-                    // print("\(count) obsidian-collecting robot collects \(count) obsidian; you now have \(mutableStock.obsidian) obsidian.")
-                case .geode:    
-                    mutableStock.geode += count     
-                    // print("\(count) geode-collecting robot collects \(count) geode; you now have \(mutableStock.geode) geode.")
-                }
-            }
-            return mutableStock
-        }
-    }
-
+    
     // MARK: - Lifecycle
 
     mutating func run() throws {
         let lines  = try String(contentsOfFile: path).components(separatedBy: .newlines)
 
-        print("Part 1: \(part1(lines))")
+        // print("Part 1: \(part1(lines))")
         print("Part 2: \(part2(lines))")
     }
 
     // MARK: - Part 1
 
     func part1(_ lines: [String]) -> Int {
-        let blueprints: [(Blueprint, [Material : Int])] = lines.compactMap(createBlueprint)
-
         var result = 0
-        for (index, blueprint) in blueprints.enumerated() {
-            let geodeMiner = GeodeMiner(blueprint: blueprint.0, maxes: blueprint.1)
-            let qualityLevel = geodeMiner.mine(robots: [.ore : 1], stock: Stock(), time: 24) * (index + 1)
-            result += qualityLevel
+        for (index, environment) in lines.compactMap(parseInput).enumerated() {
+            let temp = mineGeodes(for: 24, environment.0, environment.1) * (index + 1)
+            result += temp
+            print("Geodes mined for \(index + 1): \(temp / (index + 1))")
         }
+        
         return result
     }
 
     // MARK: - Part 2
 
     func part2(_ lines: [String]) -> Int {
-        // let blueprints = lines.compactMap(createBlueprint)
-        return 0
+        let environments = lines.compactMap(parseInput)
+        var result = 1
+        for index in 0...2 {
+            result *= mineGeodes(for: 32, environments[index].0, environments[index].1)
+        }
+        return result
     }
 
-    func createBlueprint(_ line: String) -> (Blueprint, [Material : Int])? {
-        var maxes: [Material : Int] = [.ore : 0, .clay: 0, .obsidian: 0, .geode: 0]
+    func parseInput(_ line: String) -> (Blueprint, Maxes)? {
+        var maxes: Maxes = [.ore: 0, .clay: 0, .obsidian: 0, .geode: 0]
         var blueprints = Blueprint()
         let robotsString = line.components(separatedBy: ".").filter { !$0.isEmpty }
         for (index, robotString) in robotsString.enumerated() {
@@ -180,14 +73,14 @@ struct Dec192022: ParsableCommand, AOCDay {
                       let costType = Material(rawValue: components[7])
                       else { return nil }
                 maxes[costType] = max(cost, maxes[costType]!)
-                blueprints[type] = Robot(type: type, cost: [costType : cost])
+                blueprints[type] = [costType : cost]
             } else if index == 1 {
                 guard let type = Material(rawValue: components[1]) ,
                       let cost = Int(components[4]),
                       let costType = Material(rawValue: components[5])
                 else { return nil }
                 maxes[costType] = max(cost, maxes[costType]!)
-                blueprints[type] = Robot(type: type, cost: [costType : cost])
+                blueprints[type] = [costType : cost]
             } else {
                 guard let type = Material(rawValue: components[1]) ,
                       let cost = Int(components[4]),
@@ -197,9 +90,133 @@ struct Dec192022: ParsableCommand, AOCDay {
                 else { return nil }
                 maxes[costType] = max(cost, maxes[costType]!)
                 maxes[costType2] = max(cost2, maxes[costType2]!)
-                blueprints[type] = Robot(type: type, cost: [costType : cost, costType2 : cost2])
+                blueprints[type] = [costType : cost, costType2 : cost2]
             }
         }
         return (blueprints, maxes)
     }
+
+    func mineGeodes(for time: Int, _ blueprint: Blueprint, _ maxes: Maxes) -> Int {
+        var queue = Queue<State>()
+        let start = State(time: time, robots: [.ore: 1], stock: [.ore: 0, .clay: 0, .obsidian: 0, .geode: 0], skipped: Set())
+        queue.enqueue(start)
+
+        var maxGeodes = 0
+        var best: [Int: Int] = [:]
+
+        while let state = queue.dequeue() {
+            best[state.time] = max(best[state.time] ?? 0, state.stock[.geode] ?? 0)
+            guard state.time > 0,
+                  // if we had more geodes at the current minute in previous iteration, we are not going to beat it here
+                  state.stock[.geode]! >= (best[state.time] ?? 0)
+            else {
+                maxGeodes = max(maxGeodes, state.stock[.geode]!)
+                continue
+            }
+
+            var toBuild = Material.allCases
+                .filter { canBuild($0, blueprint, state.stock) }
+                .filter { shouldBuild($0, state.robots, maxes: maxes) }
+                // if we skipped building robot last iteration, no need to try to build it here
+                .filter { !state.skipped.contains($0) }
+            // if we are able to build a geode robot, this is the only one we should build
+            if toBuild.contains(.geode) { toBuild = [.geode] }
+
+            let stock = collect(state.robots, state.stock)
+            for material in toBuild {
+                let (newRobots, newStock) = build(blueprint, material, state.robots, stock)
+                queue.enqueue(State(time: state.time - 1, robots: newRobots, stock: newStock, skipped: Set()))
+            }
+
+            // as long as we dont have option to build geode, we want to add scenario of no build to queue
+            if toBuild != [.geode] {
+                queue.enqueue(State(time: state.time - 1, robots: state.robots, stock: stock, skipped: Set(toBuild)))
+            }
+        }
+
+        return maxGeodes
+    }
+
+    func canBeatMax(_ time: Int, _ geodes: Int, _ max: Int) -> Bool {
+        return (geodes + time * (time - 1) / 2 > max)
+    }
+
+    // returns true if we can build a robot of type material based on stock
+    func canBuild(_ material: Material, _ blueprint: Blueprint, _ stock: Stock) -> Bool {
+        guard let oreStock = stock[.ore], 
+              let clayStock = stock[.clay],
+              let obsidianStock = stock[.obsidian],
+              let oreCost = blueprint[.ore]?[.ore],
+              let clayCost = blueprint[.clay]?[.ore],
+              let obsidianOreCost = blueprint[.obsidian]?[.ore],
+              let obsidianClayCost = blueprint[.obsidian]?[.clay],
+              let geodeOreCost = blueprint[.geode]?[.ore],
+              let geodeObsidianCost = blueprint[.geode]?[.obsidian]
+        else { return false }
+
+        switch material {
+            case .ore:      return oreStock >= oreCost
+            case .clay:     return oreStock >= clayCost
+            case .obsidian: return oreStock >= obsidianOreCost && clayStock >= obsidianClayCost
+            case .geode:    return oreStock >= geodeOreCost && obsidianStock >= geodeObsidianCost
+        }
+    }
+
+    // returns true if we should build a robot of type material 
+    // always true for geode
+    // true for other types if we are not yet producing enough to harvest max materials needed in one round
+    func shouldBuild(_ material: Material, _ robots: Robots, maxes: Maxes) -> Bool {
+        guard material != .geode else { return true }
+
+        guard let numberCurrentlyProducing = robots[material],
+              let maximum = maxes[material] 
+        else { return true }
+
+        return numberCurrentlyProducing < maximum
+    }
+
+    func build(_ blueprint: Blueprint, _ material: Material, _ robots: Robots, _ stock: Stock) -> (Robots, Stock) {
+        var mutableStock = stock
+        guard let oreStock = mutableStock[.ore],
+              let clayStock = mutableStock[.clay],
+              let obsidianStock = mutableStock[.obsidian],
+              let geodeStock = mutableStock[.geode] 
+        else { return (robots, stock) }
+
+        for buildMaterial in blueprint[material]!.keys {
+            guard let cost = blueprint[material]?[buildMaterial] else { continue }
+            switch buildMaterial {
+            case .ore:      mutableStock[.ore] = oreStock - cost
+            case .clay:     mutableStock[.clay] = clayStock - cost
+            case .obsidian: mutableStock[.obsidian] = obsidianStock - cost
+            case .geode:    mutableStock[.geode] = geodeStock - cost
+            }
+        }
+
+        var mutableRobots = robots
+        let currentCount = robots[material] ?? 0
+        mutableRobots[material] = currentCount + 1
+
+        return (mutableRobots, mutableStock)
+    }
+
+    func collect(_ robots: Robots, _ stock: Stock) -> Stock {
+            var mutableStock = stock
+            guard let oreStock = mutableStock[.ore],
+                  let clayStock = mutableStock[.clay],
+                  let obsidianStock = mutableStock[.obsidian],
+                  let geodeStock = mutableStock[.geode] 
+            else { return stock }
+
+            for material in robots.keys {
+                guard let count = robots[material] else { continue }
+                switch material {
+                case .ore:      mutableStock[.ore] = oreStock + count  
+                case .clay:     mutableStock[.clay] = clayStock + count  
+                case .obsidian: mutableStock[.obsidian] = obsidianStock + count
+                case .geode:    mutableStock[.geode] = geodeStock + count  
+                }
+            }
+            return mutableStock
+        }
 }
