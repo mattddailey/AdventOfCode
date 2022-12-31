2a4cc65e2d15bc15a7e929ed8415ab3026ce592b
Matt Dailey <mattdailey@Matts-Mac-mini.local>
2022-12-31 10:20:26 -0500

    Day 22 start

--- Sources/AdventOfCode/Subcommands/2022/Dec222022.swift
+++ Sources/AdventOfCode/Subcommands/2022/Dec222022.swift
@@ -1,12 +1,62 @@
 import ArgumentParser
 
 struct Dec222022: ParsableCommand, AOCDay {
-    static let configuration = CommandConfiguration(abstract: "Advent of Code - 2022 December 22", version: "1.0.0")
+    static let configuration = CommandConfiguration(abstract: "Advent of Code - Monkey Map", version: "1.0.0")
 
     @Option(name: .shortAndLong, help: "Input file path")
     var path: String = "Input/dec222022.txt"
 
     // MARK: - Data Structures
+
+    enum Move {
+        case amount(Int)
+        case left
+        case right
+    }
+
+    class MonkeyMap {
+        let tiles: Set<Point>
+        let walls: Set<Point>
+        let moves: [Move]
+
+        init(_ lines: [String]) {
+            // Build sets for walls and tiles points
+            var movesIndex: Int = 0
+            var walls = Set<Point>()
+            var tiles = Set<Point>()
+            for (y, line) in lines.enumerated() {
+                guard !line.isEmpty else { 
+                    movesIndex = y + 1
+                    break 
+                }
+
+                for (x, char) in line.enumerated() {
+                    if char == "#" {
+                        walls.insert(Point(x: x + 1, y: y + 1))
+                    } else if char == "." {
+                        tiles.insert(Point(x: x + 1, y: y + 1))
+                    }
+                }
+            }
+            self.tiles = tiles
+            self.walls = walls
+
+            // Build list of moves
+            var moves: [Move] = []
+            let numbers = lines[movesIndex].components(separatedBy: .decimalDigits.inverted)
+            let directions = lines[movesIndex].components(separatedBy: .decimalDigits).filter { !$0.isEmpty }
+            for (index, number) in numbers.enumerated() {
+                if let number = Int(number) {
+                    moves.append(.amount(number))
+                }
+
+                if index != numbers.count - 1 {
+                    directions[index] == "L" ? moves.append(.left) : moves.append(.right)
+                }
+            }
+            self.moves = moves
+        }
+    }
 
     // MARK: - Lifecycle
 
@@ -20,6 +70,7 @@ struct Dec222022: ParsableCommand, AOCDay {
     // MARK: - Part 1
 
     func part1(_ lines: [String]) -> Int {
+        let monkeyMap = MonkeyMap(lines)
         return 0
     }
 
