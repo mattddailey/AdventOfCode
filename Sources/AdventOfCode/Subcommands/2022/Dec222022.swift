3d45fa754202f1d0c077e2a18d0c8eeb85c0e816
Matt Dailey <mattdailey@Matts-Mac-mini.local>
2023-01-04 08:11:35 -0500

    Day 22 part 1

--- Sources/AdventOfCode/Subcommands/2022/Dec222022.swift
+++ Sources/AdventOfCode/Subcommands/2022/Dec222022.swift
@@ -8,54 +8,16 @@ struct Dec222022: ParsableCommand, AOCDay {
 
     // MARK: - Data Structures
 
+    enum Land {
+        case tile
+        case wall
+        case whitespace
+    }
+
     enum Move {
         case amount(Int)
         case left
         case right
-    }
-
-    class MonkeyMap {
-        let tiles: Set<Point>
-        let walls: Set<Point>
-        let moves: [Move]
-
-        init(_ lines: [String]) {
-            // Build sets for walls and tiles points
-            var movesIndex: Int = 0
-            var walls = Set<Point>()
-            var tiles = Set<Point>()
-            for (y, line) in lines.enumerated() {
-                guard !line.isEmpty else { 
-                    movesIndex = y + 1
-                    break 
-                }
-
-                for (x, char) in line.enumerated() {
-                    if char == "#" {
-                        walls.insert(Point(x: x + 1, y: y + 1))
-                    } else if char == "." {
-                        tiles.insert(Point(x: x + 1, y: y + 1))
-                    }
-                }
-            }
-            self.tiles = tiles
-            self.walls = walls
-
-            // Build list of moves
-            var moves: [Move] = []
-            let numbers = lines[movesIndex].components(separatedBy: .decimalDigits.inverted)
-            let directions = lines[movesIndex].components(separatedBy: .decimalDigits).filter { !$0.isEmpty }
-            for (index, number) in numbers.enumerated() {
-                if let number = Int(number) {
-                    moves.append(.amount(number))
-                }
-
-                if index != numbers.count - 1 {
-                    directions[index] == "L" ? moves.append(.left) : moves.append(.right)
-                }
-            }
-            self.moves = moves
-        }
     }
 
     // MARK: - Lifecycle
@@ -70,13 +32,179 @@ struct Dec222022: ParsableCommand, AOCDay {
     // MARK: - Part 1
 
     func part1(_ lines: [String]) -> Int {
-        let monkeyMap = MonkeyMap(lines)
-        return 0
+        let (map, moves) = parseInput(lines)
+        return determinePassword(map, moves)
     }
 
     // MARK: - Part 2
 
     func part2(_ lines: [String]) -> Int {
         return 0
+    }
+
+    // MARK: Shared
+
+    func determinePassword(_ map: [[Land]], _ moves: [Move]) -> Int {
+        guard let landOffset = map[0].leftMostLandIndex else { fatalError("Could not find non-whitespace land in first row of map") }
+
+        var current = Point(x: landOffset, y: 0)
+        var direction = Direction.east
+        for move in moves {
+            switch move {
+            case .amount(var remaining):
+                while remaining > 0  {
+                    remaining -= 1
+                    let next = makeMove(current, direction, map)
+                    guard map[safe: next.y]?[safe: next.x] != .wall else { break }
+                    current = next
+                }
+            case .left:
+                direction = direction.rotatedLeft
+            case .right:
+                direction = direction.rotatedRight
+            }
+        }
+
+        return (1000 * (current.y + 1)) + (4 * (current.x + 1)) + direction.rawValue
+    }
+
+    func makeMove(_ point: Point, _ direction: Direction, _ map: [[Land]]) -> Point {
+        switch direction {
+            case .north: return map.northOf(point)
+            case .south: return map.southOf(point)
+            case .east:  return map.eastOf(point)
+            case .west:  return map.westOf(point)
+        }
+    }
+
+    func parseInput(_ lines: [String]) -> ([[Land]], [Move]) {
+        guard let separatorIndex = lines.firstIndex(where: { $0.isEmpty}) else { fatalError("Input is not formatted as expected ")}
+
+        // Build map
+        var map: [[Land]] = []
+        for line in lines {
+            var row: [Land] = []
+            guard !line.isEmpty else { break }
+
+            for char in line {
+                if char == "#" {
+                    row.append(.wall)
+                } else if char == "." {
+                    row.append(.tile)
+                } else {
+                    row.append(.whitespace)
+                }
+            }
+            map.append(row)
+        }
+
+        // Build list of moves
+        var moves: [Move] = []
+        let numbers = lines[separatorIndex + 1].components(separatedBy: .decimalDigits.inverted)
+        let directions = lines[separatorIndex + 1].components(separatedBy: .decimalDigits).filter { !$0.isEmpty }
+        for (index, number) in numbers.enumerated() {
+            if let number = Int(number) {
+                moves.append(.amount(number))
+            }
+
+            if index != numbers.count - 1 {
+                directions[index] == "L" ? moves.append(.left) : moves.append(.right)
+            }
+        }
+
+        return (map, moves)
+    }
+
+    
+}
+
+extension Array where Element == Dec222022.Land {
+    var leftMostLandIndex: Int? {
+        self.firstIndex(where: { $0 != .whitespace })
+    }
+
+    var rightMostLandIndex: Int? {
+        self.count - 1
+    }
+}
+
+extension Array where Element == [Dec222022.Land] {
+    func northOf(_ point: Point) -> Point {
+        let next = self[safe: point.y - 1]?[safe: point.x]
+        if next != nil && next != .whitespace {
+            return Point(x: point.x, y: point.y - 1)
+        } else if let y = bottomMostLandIndex(column: point.x)   {
+            return Point(x: point.x, y: y)
+        } else {
+            fatalError()
+        }
+    }
+
+    func southOf(_ point: Point) -> Point {
+        let next = self[safe: point.y + 1]?[safe: point.x]
+        if next != nil && next != .whitespace {
+            return Point(x: point.x, y: point.y + 1)
+        } else if let y = topMostLandIndex(column: point.x)   {
+            return Point(x: point.x, y: y)
+        } else {
+            fatalError()
+        }
+    }
+
+    func eastOf(_ point: Point) -> Point {
+        let next = self[safe: point.y]?[safe: point.x + 1]
+        if next != nil && next != .whitespace {
+            return Point(x: point.x + 1, y: point.y)
+        } else if let x = self[safe: point.y]?.leftMostLandIndex   {
+            return Point(x: x, y: point.y)
+        } else {
+            fatalError()
+        }
+    }
+
+    func westOf(_ point: Point) -> Point {
+        let next = self[safe: point.y]?[safe: point.x - 1]
+        if next != nil && next != .whitespace {
+            return Point(x: point.x - 1, y: point.y)
+        } else if let x = self[safe: point.y]?.rightMostLandIndex   {
+            return Point(x: x, y: point.y)
+        } else {
+            fatalError()
+        }
+    }
+
+
+    func topMostLandIndex(column x: Int) -> Int? {
+        self.firstIndex(where: { 
+            $0[safe: x] != nil && $0[safe: x] != .whitespace 
+        })
+    }
+
+    func bottomMostLandIndex(column x: Int) -> Int? {
+        let reversed = Array(self.reversed())
+        if let index = reversed.firstIndex(where: { $0[safe: x] != nil && $0[safe: x] != .whitespace }) {
+            return reversed.count - 1 - index
+        }
+        return nil
+    }
+}
+
+extension Direction {
+    var rotatedLeft: Direction {
+        switch self {
+            case .north: return .west
+            case .east: return .north
+            case .south: return .east
+            case .west: return .south
+        }
+    }
+
+    var rotatedRight: Direction {
+        switch self {
+            case .north: return .east
+            case .east: return .south
+            case .south: return .west
+            case .west: return .north
+        }
     }
 }
