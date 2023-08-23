ee84372286444dd6521bbb50d21af678bd393fc4
Matt Dailey <mattddailey@gmail.com>
2023-08-22 20:29:54 -0400

    Remove cookie

--- Sources/AdventOfCode/Shared/InputDownloader/Input.swift
+++ Sources/AdventOfCode/Shared/InputDownloader/Input.swift
@@ -19,7 +19,7 @@ struct Input {
     }
     
     private let baseURL = "https://adventofcode.com/"
-    private let cookie = "_ga=GA1.2.1302195566.1692136656; _gid=GA1.2.124999209.1692136656; _gat=1; session=53616c7465645f5faa53cec1e59b7a5f2e1ab4326253e8df3d3d49edba7cd4c6904533aed047ddaeeb0c8d42968334875d12d6ba85eb4b91decae9004512a5d5; _ga_MHSNPJKWC7=GS1.2.1692136656.1.1.1692136670.0.0.0"
+    private let cookie = ""
     
     init(day: Int, year: Int) async throws {
         let urlString: String = baseURL + String(year) + "/day/" + String(day) + "/input"
