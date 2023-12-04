ab21438e845e483cfbde796eb84e3aac69af2a67
Matt Dailey <mattddailey@gmail.com>
2023-12-04 12:40:28 -0500

    Remove tests in shared package

--- AdventOfCodeShared/Package.swift
+++ AdventOfCodeShared/Package.swift
@@ -6,18 +6,12 @@ import PackageDescription
 let package = Package(
     name: "AdventOfCodeShared",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AdventOfCodeShared",
             targets: ["AdventOfCodeShared"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AdventOfCodeShared"),
-        .testTarget(
-            name: "AdventOfCodeSharedTests",
-            dependencies: ["AdventOfCodeShared"]),
+            name: "AdventOfCodeShared")
     ]
 )
