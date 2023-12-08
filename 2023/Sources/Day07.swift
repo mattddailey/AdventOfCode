1fa06dd62f639cb88f6308cfc186c79f506222ff
Matt Dailey <mattddailey@gmail.com>
2023-12-08 06:37:33 -0500

    Messy working day 7

---
+++ 2023/Sources/Day07.swift
@@ -0,0 +1,27 @@
+//  Day07.swift
+//
+//
+//  Created by Matt Dailey on 12/07/23.
+//
+
+import ArgumentParser
+import AdventOfCodeShared
+import Foundation
+
+struct Day07: AdventOfCodeDay, AsyncParsableCommand {
+  static let configuration = CommandConfiguration(abstract: "Advent of Code - December 06, 2023 - You Give A Seed A Fertilizer")
+  
+  func run() async throws {
+    print("Part 1: \(part1(""))")
+    print("Part 2: \(part2(""))")
+  }
+  
+  func part1(_ input: String) -> CustomStringConvertible {
+    ""
+  }
+  
+  func part2(_ input: String) -> CustomStringConvertible {
+    ""
+  }
+  
+}
---
+++ 2023/Tests/AdventOfCode2023Tests/Day07Tests.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Matt Dailey on 12/7/23.
+//
+
+import Foundation
