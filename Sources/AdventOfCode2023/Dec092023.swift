d24994851867195571d3dd4abe5881c5579cabf8
Matthew Dailey <matt.dailey@veeva.com>
2023-12-24 16:51:32 -0500

    Day 9

--- Sources/AdventOfCode2023/Dec092023.swift
+++ Sources/AdventOfCode2023/Dec092023.swift
@@ -5,19 +5,41 @@
 //  Created by Matt Dailey on 12/09/23.
 //
 
+import Algorithms
 import ArgumentParser
 import AdventOfCodeShared
 import AOCDay
 import Foundation
 
-@AOCDay
+@AOCDay(name: "Mirage Maintenance")
 struct Dec092023: AdventOfCodeDay, AsyncParsableCommand {
   func part1(_ input: String) throws -> CustomStringConvertible {
-    ""
+    input
+      .components(separatedBy: .newlines)
+      .map { $0.components(separatedBy: .whitespaces).compactMap { Int($0) } }
+      .map(nextValue)
+      .reduce(0, +)
   }
   
   func part2(_ input: String) throws -> CustomStringConvertible {
-    ""
+    input
+      .components(separatedBy: .newlines)
+      .map { $0.components(separatedBy: .whitespaces).compactMap { Int($0) } }
+      .map { $0.reversed() }
+      .map(nextValue)
+      .reduce(0, +)
+  }
+  
+  private func nextValue(ofSequence sequence: [Int]) -> Int {
+    guard !sequence.allSatisfy({ $0 == 0 }) else {
+      return 0
+    }
+    
+    return sequence.last! + nextValue(
+      ofSequence: sequence
+        .windows(ofCount: 2)
+        .map { $0.last! - $0.first! }
+      )
   }
 }
   
--- Tests/AdventOfCode2023Tests/Dec09Tests.swift
+++ Tests/AdventOfCode2023Tests/Dec09Tests.swift
@@ -4,15 +4,18 @@ import XCTest
 final class Dec09Tests: XCTestCase {
   static let input =
   """
+  0 3 6 9 12 15
+  1 3 6 10 15 21
+  10 13 16 21 30 45
   """
   
   func testDec09Part1() throws {
     let day = Dec092023()
-    XCTAssertEqual(try day.part1(Self.input) as? Int, nil)
+    XCTAssertEqual(try day.part1(Self.input) as? Int, 114)
   }
     
   func testDec09Part2() throws {
     let day = Dec092023()
-    XCTAssertEqual(try day.part2(Self.input) as? Int, nil)
+    XCTAssertEqual(try day.part2(Self.input) as? Int, 2)
   }
 }
